4b7abf39b98f25ec9516ddff16375e9b6a11e55e
kevinnika1 <87488963+kevinnika1@users.noreply.github.com>
2021-08-31 15:24:21 +0100

    Update trivyjson.py

--- trivyjson.py
+++ trivyjson.py
@@ -14,19 +14,17 @@ try:
                 thisdict= {}
                 thisdict['path']=path
                 thisdict['message']=str("Installed version: "+v.get('InstalledVersion')+", Fixed Version: "+v.get('FixedVersion'))
-                
                 if (v['Severity']=="UNKNOWN" or v['Severity']=="MEDIUM" or v['Severity']=="LOW"):
                     thisdict['level']="notice"
                 elif (v['Severity']=="HIGH"):
                     thisdict['level']="warning"
                 elif (v['Severity']=="CRITICAL"):
                     thisdict['level']="failure"
-                    
                 thisdict['line']=0
                 if ("Title" in v):
-                    thisdict['title']=str(v.get('VulnerabilityID')+", "+v.get('Title'))
+                    thisdict['title']=str(v.get('VulnerabilityID')+", "+v.get('Title')+", "+"Severity: "+v.get('Severity'))
                 else:
-                    thisdict['title']=v.get('VulnerabilityID')
+                    thisdict['title']=v.get('VulnerabilityID'+", Severity: "+v.get('Severity'))
                 result.append(thisdict)
 
     with open("trivyparsedresults.json", "w") as outfile:
